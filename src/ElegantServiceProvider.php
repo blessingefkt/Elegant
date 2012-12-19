85f40f00ebc5d79fa5e1d23f43a4c15bf1dca4ca
Blessing Osakue <support@iyoworks.com>
2012-12-19 03:24:18 -0600

    Moved service provider to src directory

---
+++ src/ElegantServiceProvider.php
@@ -0,0 +1,30 @@
+<?php namespace IyoWorks\Elegant;
+use Illuminate\Support\ServiceProvider;
+
+define('ELEGANT_VERSION', '1.0.0');
+
+class ElegantServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+
+	}
+
+	/**
+	 * Boot the service provider.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+
+	}
+
+
+
+}
