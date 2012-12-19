d43a28f32150cc858d095644fe2d667e0562903a
Blessing Osakue <support@iyoworks.com>
2012-12-19 02:20:01 -0600

    Initial Commit

---
+++ ElegantServiceProvider.php
@@ -0,0 +1,36 @@
+<?php namespace IyoWorks\Elegant;
+
+use Illuminate\Support\ServiceProvider;
+
+define('ELEGANT_VERSION', '2.0.0');
+
+class ElegantServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		// Register the package configuration with the loader.
+		// $this->app['config']->package('jasonlewis/elegant', __DIR__.'/../config');
+
+		// Because Laravel doesn't actually set a public path here we'll define out own. This may become
+		// a limitation and hopefully will change at a later date.
+		$this->app['path.public'] = realpath($this->app['path.base'].'/packages/iyoworks/elegant');
+	}
+
+	/**
+	 * Boot the service provider.
+	 *
+	 * @return void
+	 */
+	// public function boot()
+	// {
+	// 	$this->registerRoutes();
+	// }
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Elegant
+=======
+
+An Extension of illuminate's Eloquent Model. Made for Laravel 4
+
+**First time using composer: Trying to port my laravel 3 extension of eloquent to laravel 4.**
